8e0c88d502d2123211f34e18a3c67258dc4ebded
Aleksandar Georgiev <100850331+Sash0G@users.noreply.github.com>
2024-04-15 11:40:42 +0300

    First commit

---
+++ EQUATION/Main.py
@@ -0,0 +1,3 @@
+import OPZ
+a= '20/8*8-(2+3)*10'
+print(OPZ.OPZ(a))
--- EQUATION/OPZ.py
+++ EQUATION/OPZ.py
@@ -1,12 +1,52 @@
-def 
+def priority(op):
+    if op=='+' or op=='-':
+        return 1
+    elif op=='*' or op=='/':
+        return 2
+    else:
+        return 0
+    
+def operation(a,b,c):
+    if c=='+':
+        return a+b
+    elif c=='-':
+        return b-a
+    elif c=='*':
+        return a*b
+    else:
+        return b/a
 
 def OPZ(input):
-    stack = []
-    for i in range(len(input)):
+    stack = [0]
+    op = []
+    i=0
+    while i<len(input):
         if input[i].isdigit():
             a = 0
-            while input[i].isdigit():
-                a = a*10+input[i]-'0'
+            while i<len(input) and input[i].isdigit():
+                a = a*10+int(input[i])
                 i=i+1
+            
             stack.append(a)
-        elif input[]
+            i-=1
+        
+        elif priority(input[i])>0:
+            while len(op) > 0 and priority(input[i])<=priority(op[-1]):
+                stack.append(operation(stack.pop(),stack.pop(),op.pop()))
+
+            op.append(input[i])
+
+        elif input[i]=='(':
+            op.append('(')
+
+        else:
+            while op[-1]!='(':
+                stack.append(operation(stack.pop(),stack.pop(),op.pop()))
+
+            op.pop()
+        i+=1
+    while len(op) > 0:
+        stack.append(operation(stack.pop(),stack.pop(),op.pop()))
+
+    return stack[-1]
+                
